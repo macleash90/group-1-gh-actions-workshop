71d87091a97310922ec30ad45dc1afa52bb2284e
Fred Maclean <macleash90@gmail.com>
2026-08-05 19:42:44 +0200

    Enhance coverage reporting in CI workflow and Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,12 @@ export default defineConfig({
     coverage: {
       reporter: ["text", "json", "json-summary"],
       reportOnFailure: true,
+      thresholds: {
+        lines: 100,
+        branches: 100,
+        functions: 100,
+        statements: 100
+      }
     },
     setupFiles: ["./test/setup.ts"],
   },
